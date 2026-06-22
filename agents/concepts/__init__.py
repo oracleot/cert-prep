6dfe1e745fed6c14d34b70f3df9812358d36d909
Dami <doduronbi@gmail.com>
2026-06-22 19:57:32 +0100

    9.3: re-export NoReadyConcept and selectors from concepts package

--- agents/concepts/__init__.py
+++ agents/concepts/__init__.py
@@ -1,13 +1,17 @@
 """Public surface of the concepts package."""
 from .loader import filter_ready, find_concept, load_all_concepts
 from .schema import Concept
+from .selector import NoReadyConcept, select_initial_concept, select_rechallenge_concept
 from .validate import ConceptValidation, validate_concept
 
 __all__ = [
     "Concept",
     "ConceptValidation",
+    "NoReadyConcept",
     "filter_ready",
     "find_concept",
     "load_all_concepts",
+    "select_initial_concept",
+    "select_rechallenge_concept",
     "validate_concept",
 ]
